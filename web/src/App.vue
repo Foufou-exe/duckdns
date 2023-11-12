42eed7fd5b0b10ea4cae3107459a3ac6f122f1e8
Foufou-exe <thibaut.maurras34@gmail.com>
2023-11-11 19:39:36 -0500

    MODIF LAST MINUTE

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,39 @@
-<template>
+<!-- <template>
     <DefaultNavbar v-if="!isAdminRoute"/>
     <DefaultViews />
     <DefaultFooter v-if="!isAdminRoute"/>
+</template>
+
+<script setup>
+// Vue Composables
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+// Vue Main Components
+import DefaultNavbar from '@layouts/DefaultNavbar.vue'
+import DefaultViews from '@layouts/DefaultViews.vue'
+import DefaultFooter from '@layouts/DefaultFooter.vue' 
+
+// Import the store
+import { useImageStore } from '@store/store.js';
+
+// Variables
+const route = useRoute();
+const isAdminRoute = computed(() => route.meta.admin || route.path.startsWith('/admin'));
+
+const imageStore = useImageStore();
+imageStore.initializeImage();
+</script> -->
+
+<template>
+    <div v-if="!isAdminRoute">
+        <DefaultNavbar/>
+        <DefaultViews />
+        <DefaultFooter/>
+    </div>
+    <div v-else>
+        <router-view />
+    </div>
 </template>
 
 <script setup>
--- web/src/layouts/DefaultViews.vue
+++ web/src/layouts/DefaultViews.vue
@@ -1,23 +1,13 @@
 <template>
-    <div v-if="!isAdminRoute" class="flex flex-col justify-center pl-10 pr-10 pt-5 min-h-screen">
+    <div class="flex flex-col justify-center pl-10 pr-10 pt-5 min-h-screen">
       <div class="w-full max-w-5xl mx-auto">
           <router-view />
           <BackToTopButton />
       </div>
     </div>
-    <div v-else>
-      <!-- Structure spécifique pour les routes admin -->
-      <router-view />
-      <!-- Vous pouvez ajouter d'autres éléments spécifiques à l'admin ici -->
-    </div>
 </template>
   
 <script setup>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
 import BackToTopButton from '@components/BackToTopButton.vue';
-
-const route = useRoute();
-const isAdminRoute = computed(() => route.meta.admin || route.path.startsWith('/admin'));
 </script>
   
